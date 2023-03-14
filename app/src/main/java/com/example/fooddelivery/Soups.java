9a8273129d924d6351ee1ed0439b3429b6173d79
Suresh <suresh.sanganaboyana@improve10x.com>
2023-03-14 15:29:50 +0530

    Created dummy data

---
+++ app/src/main/java/com/example/fooddelivery/Soups.java
@@ -0,0 +1,18 @@
+package com.example.fooddelivery;
+
+public class Soups {
+    public String typeOfSoup;
+    public String details;
+    public String servingNumber;
+    public String weightGr;
+
+    public Soups() {
+    }
+
+    public Soups(String typeOfSoup, String details, String servingNumber, String weightGr) {
+        this.typeOfSoup = typeOfSoup;
+        this.details = details;
+        this.servingNumber = servingNumber;
+        this.weightGr = weightGr;
+    }
+}
--- app/src/main/java/com/example/fooddelivery/SoupsActivity.java
+++ app/src/main/java/com/example/fooddelivery/SoupsActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class SoupsActivity extends AppCompatActivity {
+
+    private ArrayList<Soups> soupList = new ArrayList<Soups>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soups);
+        setupData();
+    }
+
+    public void setupData() {
+        Soups soups = new Soups();
+        soups.typeOfSoup = "Chicken soup";
+        soups.details = "Chicken, vegetables, butter, tiny pasta, green beans, carrots,...";
+        soups.servingNumber = "2";
+        soups.weightGr = "345gr";
+        soupList.add(soups);
+
     }
 }
