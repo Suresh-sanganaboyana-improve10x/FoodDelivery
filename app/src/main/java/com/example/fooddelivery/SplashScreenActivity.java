50c4b18bf7659dc39659374b7b629905b8ea7156
Suresh <suresh.sanganaboyana@improve10x.com>
2023-03-14 14:28:00 +0530

    created food delivery project and pushed into GitHub

---
+++ app/src/main/java/com/example/fooddelivery/SplashScreenActivity.java
@@ -0,0 +1,23 @@
+package com.example.fooddelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(this, );
+            startActivity(intent);
+        }, 3000);
+    }
+
+}
